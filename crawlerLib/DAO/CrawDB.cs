8755afb052ad7ebd1df0724e92843c75aa0ee21b
jotalima72 <48960188+jotalima72@users.noreply.github.com>
2020-09-27 04:08:16 -0300

    librarie

---
+++ crawlerLib/DAO/CrawDB.cs
@@ -0,0 +1,33 @@
+﻿using crawlerLib.models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace crawlerLib.DAO
+{
+    public class CrawDB : DbContext
+    {
+        public DbSet<Noticia> Noticias { get; set; }
+
+        public CrawDB()
+        {
+            iniciar();
+        }
+
+        private void iniciar()
+        {
+            this.Database.EnsureCreated();
+            Console.WriteLine("Banco iniciado");
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseOracle("Data Source=localhost;User Id=C##JOAO;Password=123;");
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Noticia>()
+                .HasIndex(n => n.Link)
+                .IsUnique();
+        }
+    }
+}
---
+++ crawlerLib/models/Noticia.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace crawlerLib.models
+{
+    public class Noticia
+    {
+        public int ID { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public string Link { get; set; }
+
+        public override string ToString()
+        {
+            return "Titulo - " + Titulo + "\nLink - " + Link +"\n\n";
+        }
+   
+    }
+}
